19b33a71e2900faceaf8566e0a2c1945d49cc7d5
Ismail <smilekaraman@hotmail.com>
2025-11-26 23:39:47 +0300

    create langmap.js update index.html
    index.html içinde eklenenler:
    navbar içinde toggle butonu
    head içinde stiller
    en altta toogle scripti

---
+++ js/lang-map.js
@@ -0,0 +1,36 @@
+// /js/lang-map.js
+const langMap = {
+    // root / index
+    "/": "/EN/",
+    "/index.html": "/EN/index.html",
+    "/EN/": "/",
+    "/EN/index.html": "/index.html",
+
+    // (TR -> EN)
+    "/hakkimizda.html": "/EN/about.html",
+    "/iletisim.html": "/EN/contact.html",
+    "/fotogaleri.html": "/EN/photogallery.html",
+    "/kadromuz.html": "/EN/team.html",
+    "/tedaviler/implant-tedavisi.html": "/EN/treatments/implantology.html",
+    "/tedaviler/dis-poliklinigi.html": "/EN/treatments/dental-polyclinic.html",
+    "/tedaviler/ortodonti.html": "/EN/treatments/orthodontics.html",
+    "/tedaviler/pediatri.html": "/EN/treatments/pediatrics.html",
+    "/tedaviler/pedodonti.html": "/EN/treatments/pedodontics.html",
+    "/tedaviler/periodontoloji.html": "/EN/treatments/periodontology.html",
+    "/tedaviler/tedaviler.html": "/EN/treatments/treatments.html",
+    
+
+
+    // (EN -> TR)
+    "/EN/about.html": "/hakkimizda.html",
+    "/EN/contact.html": "/iletisim.html",
+    "/EN/photogallery.html" : "/fotogaleri.html",
+    "/EN/team.html" : "/kadromuz.html",
+    "/EN/treatments/implantology.html" : "/tedaviler/implant-tedavisi.html",
+    "/EN/treatments/dental-polyclinic.html" : "/tedaviler/dis-poliklinigi.html",
+    "/EN/treatments/orthodontics.html" : "/tedaviler/ortodonti.html",
+    "/EN/treatments/pediatrics.html" : "/tedaviler/pediatri.html",
+    "/EN/treatments/pedodontics.html" : "/tedaviler/pedodonti.html",
+    "/EN/treatments/periodontology.html" : "/tedaviler/periodontoloji.html",
+    "/EN/treatments/treatments.html" : "/tedaviler/tedaviler.html",
+};
