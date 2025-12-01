a9523d5150cad21c39bfa66e4a8ee3364f962ca5
Ismail <smilekaraman@hotmail.com>
2025-12-01 09:14:36 +0300

    Update lang-map.js

--- js/lang-map.js
+++ js/lang-map.js
@@ -18,7 +18,18 @@ const langMap = {
     "/tedaviler/pedodonti.html": "/EN/treatments/pedodontics.html",
     "/tedaviler/periodontoloji.html": "/EN/treatments/periodontology.html",
     "/tedaviler/tedaviler.html": "/EN/treatments/treatments.html",
-    
+    "/blog/blog.html":"/EN/blog/blog.html",
+    "/blog/dis-beyazlatma-ne-kadar-kalici.html":"/EN/blog/how-long-does-teeth-whitening-last.html",
+    "/blog/dis-implanti-tedavisi-ne-kadar-surer.html":"/EN/blog/how-long-does-dental-implant-treatment-take.html",
+    "/blog/dis-koprusu-nedir-nasil-yapilir.html":"/EN/blog/what-is-a-dental-bridge-how-is-it-made.html",
+    "/blog/dis-tasi-nedir-nasil-temizlenir.html":"/EN/blog/what-is-dental-tartar-how-is-it-cleaned.html",
+    "/blog/lamine-dis-kaplama.html":"/EN/blog/dental-laminates.html",
+    "/blog/porselen-dis-bakimi-nasil-yapilir.html":"/EN/blog/how-to-care-fot-porcelain-teeth.html",
+    "/blog/protez-dis-nedir-nasil-yapilir.html":"/EN/blog/what-are-dentures-how-are-they-applied.html",
+    "/blog/zirkonyum-dis-kaplama-nedir-nasil-yapilir.html":"/EN/blog/what-is-zirkonium-dental-veneer-how-is-it-applied.html",
+    "/blog/zirkonyum-dis-ne-kadar-surede-yapilir.html":"/EN/blog/how-long-does-zirkonium-dental-application-take.html",
+
+
 
 
     // (EN -> TR)
@@ -33,4 +44,15 @@ const langMap = {
     "/EN/treatments/pedodontics.html" : "/tedaviler/pedodonti.html",
     "/EN/treatments/periodontology.html" : "/tedaviler/periodontoloji.html",
     "/EN/treatments/treatments.html" : "/tedaviler/tedaviler.html",
+    "/EN/blog/blog.html":"/blog/blog.html",
+    "/EN/blog/how-long-does-teeth-whitening-last.html":"/blog/dis-beyazlatma-ne-kadar-kalici.html",
+    "/EN/blog/how-long-does-dental-implant-treatment-take.html":"/blog/dis-implanti-tedavisi-ne-kadar-surer.html",
+    "/EN/blog/what-is-a-dental-bridge-how-is-it-made.html":"/blog/dis-koprusu-nedir-nasil-yapilir.html",
+    "/EN/blog/what-is-dental-tartar-how-is-it-cleaned.html":"/blog/dis-tasi-nedir-nasil-temizlenir.html",
+    "/EN/blog/dental-laminates.html":"/blog/lamine-dis-kaplama.html",
+    "/EN/blog/how-to-care-fot-porcelain-teeth.html":"/blog/porselen-dis-bakimi-nasil-yapilir.html",
+    "/EN/blog/what-are-dentures-how-are-they-applied.html":"/blog/protez-dis-nedir-nasil-yapilir.html",
+    "/EN/blog/what-is-zirkonium-dental-veneer-how-is-it-applied.html":"/blog/zirkonyum-dis-kaplama-nedir-nasil-yapilir.html",
+    "/EN/blog/how-long-does-zirkonium-dental-application-take.html":"/blog/zirkonyum-dis-ne-kadar-surede-yapilir.html",
+
 };
