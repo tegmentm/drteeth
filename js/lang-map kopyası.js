053eee9f1a9b1e02405931b83e89129395ab9af2
Ismail <smilekaraman@hotmail.com>
2025-12-08 21:04:37 +0300

    dil calismasi

---
+++ js/lang-map kopyası.js
@@ -0,0 +1,58 @@
+// /js/lang-map.js
+const langMap = {
+    // root / index
+    "/": "/EN/",
+    "/index.html": "/EN/index.html",
+    "/EN/": "/",
+    "/EN/index.html": "/index.html",
+
+    // (TR -> EN)
+    "/hakkimizda.html": "/EN/about.html",
+    "/iletisim.html": "/EN/contact.html",
+    "/fotogaleri.html": "/EN/photogallery.html",
+    "/kadromuz.html": "/EN/team.html",
+    "/tedaviler/implant-tedavisi.html": "/EN/treatments/implantology.html",
+    "/tedaviler/dis-poliklinigi.html": "/EN/treatments/dental-polyclinic.html",
+    "/tedaviler/ortodonti.html": "/EN/treatments/orthodontics.html",
+    "/tedaviler/pediatri.html": "/EN/treatments/pediatrics.html",
+    "/tedaviler/pedodonti.html": "/EN/treatments/pedodontics.html",
+    "/tedaviler/periodontoloji.html": "/EN/treatments/periodontology.html",
+    "/tedaviler/tedaviler.html": "/EN/treatments/treatments.html",
+    "/blog/blog.html":"/EN/blog/blog.html",
+    "/blog/dis-beyazlatma-ne-kadar-kalici.html":"/EN/blog/how-long-does-teeth-whitening-last.html",
+    "/blog/dis-implanti-tedavisi-ne-kadar-surer.html":"/EN/blog/how-long-does-dental-implant-treatment-take.html",
+    "/blog/dis-koprusu-nedir-nasil-yapilir.html":"/EN/blog/what-is-a-dental-bridge-how-is-it-made.html",
+    "/blog/dis-tasi-nedir-nasil-temizlenir.html":"/EN/blog/what-is-dental-tartar-how-is-it-cleaned.html",
+    "/blog/lamine-dis-kaplama.html":"/EN/blog/dental-laminates.html",
+    "/blog/porselen-dis-bakimi-nasil-yapilir.html":"/EN/blog/how-to-care-fot-porcelain-teeth.html",
+    "/blog/protez-dis-nedir-nasil-yapilir.html":"/EN/blog/what-are-dentures-how-are-they-applied.html",
+    "/blog/zirkonyum-dis-kaplama-nedir-nasil-yapilir.html":"/EN/blog/what-is-zirkonium-dental-veneer-how-is-it-applied.html",
+    "/blog/zirkonyum-dis-ne-kadar-surede-yapilir.html":"/EN/blog/how-long-does-zirkonium-dental-application-take.html",
+
+
+
+
+    // (EN -> TR)
+    "/EN/about.html": "/hakkimizda.html",
+    "/EN/contact.html": "/iletisim.html",
+    "/EN/photogallery.html" : "/fotogaleri.html",
+    "/EN/team.html" : "/kadromuz.html",
+    "/EN/treatments/implantology.html" : "/tedaviler/implant-tedavisi.html",
+    "/EN/treatments/dental-polyclinic.html" : "/tedaviler/dis-poliklinigi.html",
+    "/EN/treatments/orthodontics.html" : "/tedaviler/ortodonti.html",
+    "/EN/treatments/pediatrics.html" : "/tedaviler/pediatri.html",
+    "/EN/treatments/pedodontics.html" : "/tedaviler/pedodonti.html",
+    "/EN/treatments/periodontology.html" : "/tedaviler/periodontoloji.html",
+    "/EN/treatments/treatments.html" : "/tedaviler/tedaviler.html",
+    "/EN/blog/blog.html":"/blog/blog.html",
+    "/EN/blog/how-long-does-teeth-whitening-last.html":"/blog/dis-beyazlatma-ne-kadar-kalici.html",
+    "/EN/blog/how-long-does-dental-implant-treatment-take.html":"/blog/dis-implanti-tedavisi-ne-kadar-surer.html",
+    "/EN/blog/what-is-a-dental-bridge-how-is-it-made.html":"/blog/dis-koprusu-nedir-nasil-yapilir.html",
+    "/EN/blog/what-is-dental-tartar-how-is-it-cleaned.html":"/blog/dis-tasi-nedir-nasil-temizlenir.html",
+    "/EN/blog/dental-laminates.html":"/blog/lamine-dis-kaplama.html",
+    "/EN/blog/how-to-care-fot-porcelain-teeth.html":"/blog/porselen-dis-bakimi-nasil-yapilir.html",
+    "/EN/blog/what-are-dentures-how-are-they-applied.html":"/blog/protez-dis-nedir-nasil-yapilir.html",
+    "/EN/blog/what-is-zirkonium-dental-veneer-how-is-it-applied.html":"/blog/zirkonyum-dis-kaplama-nedir-nasil-yapilir.html",
+    "/EN/blog/how-long-does-zirkonium-dental-application-take.html":"/blog/zirkonyum-dis-ne-kadar-surede-yapilir.html",
+
+};
--- js/lang-map.js
+++ js/lang-map.js
@@ -1,58 +1,258 @@
-// /js/lang-map.js
 const langMap = {
-    // root / index
-    "/": "/EN/",
-    "/index.html": "/EN/index.html",
-    "/EN/": "/",
-    "/EN/index.html": "/index.html",
+    tr: {
+        prefix: "/",
+        pages: {
 
-    // (TR -> EN)
-    "/hakkimizda.html": "/EN/about.html",
-    "/iletisim.html": "/EN/contact.html",
-    "/fotogaleri.html": "/EN/photogallery.html",
-    "/kadromuz.html": "/EN/team.html",
-    "/tedaviler/implant-tedavisi.html": "/EN/treatments/implantology.html",
-    "/tedaviler/dis-poliklinigi.html": "/EN/treatments/dental-polyclinic.html",
-    "/tedaviler/ortodonti.html": "/EN/treatments/orthodontics.html",
-    "/tedaviler/pediatri.html": "/EN/treatments/pediatrics.html",
-    "/tedaviler/pedodonti.html": "/EN/treatments/pedodontics.html",
-    "/tedaviler/periodontoloji.html": "/EN/treatments/periodontology.html",
-    "/tedaviler/tedaviler.html": "/EN/treatments/treatments.html",
-    "/blog/blog.html":"/EN/blog/blog.html",
-    "/blog/dis-beyazlatma-ne-kadar-kalici.html":"/EN/blog/how-long-does-teeth-whitening-last.html",
-    "/blog/dis-implanti-tedavisi-ne-kadar-surer.html":"/EN/blog/how-long-does-dental-implant-treatment-take.html",
-    "/blog/dis-koprusu-nedir-nasil-yapilir.html":"/EN/blog/what-is-a-dental-bridge-how-is-it-made.html",
-    "/blog/dis-tasi-nedir-nasil-temizlenir.html":"/EN/blog/what-is-dental-tartar-how-is-it-cleaned.html",
-    "/blog/lamine-dis-kaplama.html":"/EN/blog/dental-laminates.html",
-    "/blog/porselen-dis-bakimi-nasil-yapilir.html":"/EN/blog/how-to-care-fot-porcelain-teeth.html",
-    "/blog/protez-dis-nedir-nasil-yapilir.html":"/EN/blog/what-are-dentures-how-are-they-applied.html",
-    "/blog/zirkonyum-dis-kaplama-nedir-nasil-yapilir.html":"/EN/blog/what-is-zirkonium-dental-veneer-how-is-it-applied.html",
-    "/blog/zirkonyum-dis-ne-kadar-surede-yapilir.html":"/EN/blog/how-long-does-zirkonium-dental-application-take.html",
+            "index.html": {
+                en: "/EN/index.html",
+                de: "/de/index.html"
+            },
 
+            "hakkimizda.html": {
+                en: "/EN/about.html",
+                de: "/de/uber-uns.html"
+            },
 
+            "fotogaleri.html": {
+                en: "/EN/photogallery.html",
+                de: "/DE/"
+            },
 
+            "kadromuz.html": {
+                en: "/EN/team.html",
+                de: "/DE/"
+            },
 
-    // (EN -> TR)
-    "/EN/about.html": "/hakkimizda.html",
-    "/EN/contact.html": "/iletisim.html",
-    "/EN/photogallery.html" : "/fotogaleri.html",
-    "/EN/team.html" : "/kadromuz.html",
-    "/EN/treatments/implantology.html" : "/tedaviler/implant-tedavisi.html",
-    "/EN/treatments/dental-polyclinic.html" : "/tedaviler/dis-poliklinigi.html",
-    "/EN/treatments/orthodontics.html" : "/tedaviler/ortodonti.html",
-    "/EN/treatments/pediatrics.html" : "/tedaviler/pediatri.html",
-    "/EN/treatments/pedodontics.html" : "/tedaviler/pedodonti.html",
-    "/EN/treatments/periodontology.html" : "/tedaviler/periodontoloji.html",
-    "/EN/treatments/treatments.html" : "/tedaviler/tedaviler.html",
-    "/EN/blog/blog.html":"/blog/blog.html",
-    "/EN/blog/how-long-does-teeth-whitening-last.html":"/blog/dis-beyazlatma-ne-kadar-kalici.html",
-    "/EN/blog/how-long-does-dental-implant-treatment-take.html":"/blog/dis-implanti-tedavisi-ne-kadar-surer.html",
-    "/EN/blog/what-is-a-dental-bridge-how-is-it-made.html":"/blog/dis-koprusu-nedir-nasil-yapilir.html",
-    "/EN/blog/what-is-dental-tartar-how-is-it-cleaned.html":"/blog/dis-tasi-nedir-nasil-temizlenir.html",
-    "/EN/blog/dental-laminates.html":"/blog/lamine-dis-kaplama.html",
-    "/EN/blog/how-to-care-fot-porcelain-teeth.html":"/blog/porselen-dis-bakimi-nasil-yapilir.html",
-    "/EN/blog/what-are-dentures-how-are-they-applied.html":"/blog/protez-dis-nedir-nasil-yapilir.html",
-    "/EN/blog/what-is-zirkonium-dental-veneer-how-is-it-applied.html":"/blog/zirkonyum-dis-kaplama-nedir-nasil-yapilir.html",
-    "/EN/blog/how-long-does-zirkonium-dental-application-take.html":"/blog/zirkonyum-dis-ne-kadar-surede-yapilir.html",
+            "tedaviler/tedaviler.html": {
+                en: "/EN/treatments/treatments.html",
+                de: "/DE/"
+            },
 
+            "tedaviler/dis-poliklinigi.html": {
+                en: "/EN/treatments/dental-polyclinic.html",
+                de: "/DE/"
+            },
+
+            "tedaviler/implant-tedavisi.html": {
+                en: "/EN/treatments/implantology.html",
+                de: "/DE/"
+            },
+
+            "tedaviler/ortodonti.html": {
+                en: "/EN/treatments/orthodontics.html",
+                de: "/DE/"
+            },
+
+            "tedaviler/pediatri.html": {
+                en: "/EN/treatments/pediatrics.html",
+                de: "/DE/"
+            },
+
+            "tedaviler/pedodonti.html": {
+                en: "/EN/treatments/pedodontics.html",
+                de: "/DE/"
+            },
+
+            "tedaviler/periodontoloji.html": {
+                en: "/EN/treatments/periodontology.html",
+                de: "/DE/"
+            },
+
+            "blog/blog.html": {
+                en: "/EN/blog/blog.html",
+                de: "/DE/"
+            },
+
+            "blog/dis-beyazlatma-ne-kadar-kalici.html": {
+                en: "/EN/blog/how-long-does-teeth-whitening-last.html",
+                de: "/DE/"
+            },
+
+            "blog/dis-implanti-tedavisi-ne-kadar-surer.html": {
+                en: "/EN/blog/how-long-does-dental-implant-treatment-take.html",
+                de: "/DE/"
+            },
+
+            "blog/dis-koprusu-nedir-nasil-yapilir.html": {
+                en: "/EN/blog/what-is-a-dental-bridge-how-is-it-made.html",
+                de: "/DE/"
+            },
+
+            "blog/dis-tasi-nedir-nasil-temizlenir.html": {
+                en: "/EN/blog/what-is-dental-tartar-how-is-it-cleaned.html",
+                de: "/DE/"
+            },
+
+            "blog/lamine-dis-kaplama.html": {
+                en: "/EN/blog/dental-laminates.html",
+                de: "/DE/"
+            },
+
+            "blog/porselen-dis-bakimi-nasil-yapilir.html": {
+                en: "/EN/blog/how-to-care-fot-porcelain-teeth.html",
+                de: "/DE/"
+            },
+
+            "blog/protez-dis-nedir-nasil-yapilir.html": {
+                en: "/EN/blog/what-are-dentures-how-are-they-applied.html",
+                de: "/DE/"
+            },
+
+            "blog/zirkonyum-dis-kaplama-nedir-nasil-yapilir.html": {
+                en: "/EN/blog/what-is-zirkonium-dental-veneer-how-is-it-applied.html",
+                de: "/DE/"
+            },
+
+            "blog/zirkonyum-dis-ne-kadar-surede-yapilir.html": {
+                en: "/EN/blog/how-long-does-zirkonium-dental-application-take.html",
+                de: "/DE/"
+            }
+        }
+    },
+
+    en: {
+        prefix: "/EN/",
+        pages: {
+
+            "index.html": {
+                tr: "/index.html",
+                de: "/DE/index.html"
+            },
+
+            "about.html": {
+                tr: "/hakkimizda.html",
+                de: "/DE/"
+            },
+
+            "photogallery.html": {
+                tr: "/fotogaleri.html",
+                de: "/DE/"
+            },
+
+            "team.html": {
+                tr: "/kadromuz.html",
+                de: "/DE/"
+            },
+
+            "treatments/treatments.html": {
+                tr: "/tedaviler/tedaviler.html",
+                de: "/DE/"
+            },
+
+            "treatments/dental-polyclinic.html": {
+                en: "/tedaviler/dis-poliklinigi.html",
+                de: "/DE/"
+            },
+
+            "treatments/implantology.html": {
+                en: "/tedaviler/implant-tedavisi.html",
+                de: "/DE/"
+            },
+
+            "treatments/orthodontics.html": {
+                en: "/tedaviler/ortodonti.html",
+                de: "/DE/"
+            },
+
+            "treatments/pediatrics.html": {
+                en: "/tedaviler/pediatri.html",
+                de: "/DE/"
+            },
+
+            "treatments/pedodontics.html": {
+                en: "/tedaviler/pedodonti.html",
+                de: "/DE/"
+            },
+
+            "treatments/periodontology.html": {
+                en: "/tedaviler/periodontoloji.html",
+                de: "/DE/"
+            },
+
+            "blog/blog.html": {
+                en: "/blog/blog.html",
+                de: "/DE/"
+            },
+
+            "blog/how-long-does-teeth-whitening-last.html": {
+                en: "/blog/dis-beyazlatma-ne-kadar-kalici.html",
+                de: "/DE/"
+            },
+
+            "blog/how-long-does-dental-implant-treatment-take.html": {
+                en: "/blog/dis-implanti-tedavisi-ne-kadar-surer.html",
+                de: "/DE/"
+            },
+
+            "blog/what-is-a-dental-bridge-how-is-it-made.html": {
+                en: "/blog/dis-koprusu-nedir-nasil-yapilir.html",
+                de: "/DE/"
+            },
+
+            "blog/what-is-dental-tartar-how-is-it-cleaned.html": {
+                en: "/blog/dis-tasi-nedir-nasil-temizlenir.html",
+                de: "/DE/"
+            },
+
+            "blog/dental-laminates.html": {
+                en: "/blog/lamine-dis-kaplama.html",
+                de: "/DE/"
+            },
+
+            "blog/how-to-care-fot-porcelain-teeth.html": {
+                en: "/blog/porselen-dis-bakimi-nasil-yapilir.html",
+                de: "/DE/"
+            },
+
+            "blog/what-are-dentures-how-are-they-applied.html": {
+                en: "/blog/protez-dis-nedir-nasil-yapilir.html",
+                de: "/DE/"
+            },
+
+            "blog/what-is-zirkonium-dental-veneer-how-is-it-applied.html": {
+                en: "/blog/zirkonyum-dis-kaplama-nedir-nasil-yapilir.html",
+                de: "/DE/"
+            },
+
+            "blog/how-long-does-zirkonium-dental-application-take.html": {
+                en: "/blog/zirkonyum-dis-ne-kadar-surede-yapilir.html",
+                de: "/DE/"
+            }
+
+        }
+    },
+
+    de: {
+        prefix: "/DE/",
+        pages: {
+            "index.html": {
+                tr: "/index.html",
+                en: "/en/index.html"
+            },
+            "uber-uns.html": {
+                tr: "/hakkimizda.html",
+                en: "/en/about.html"
+            }
+        }
+    }
 };
+
+// --- Dil değiştirme fonksiyonu ---
+function switchLanguage(targetLang) {
+    const currentUrl = window.location.pathname;
+
+    // aktif dili bul
+    let currentLang = "tr";
+    if (currentUrl.startsWith("/en/")) currentLang = "en";
+    if (currentUrl.startsWith("/de/")) currentLang = "de";
+
+    // sayfa adını ayıkla
+    const pageName = currentUrl.replace(langMap[currentLang].prefix, "");
+
+    // hedef dilde karşılığı var mı?
+    const mapped =
+        langMap[currentLang].pages[pageName]?. [targetLang] ||
+        langMap[targetLang].prefix; // fallback: hedef dil ana sayfa
+
+    window.location.href = mapped;
+}
